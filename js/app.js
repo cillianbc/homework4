c88c89221d703f6d6c9a77f14797df9cd99e0423
Cillianbc <cillianbc@gmail.com>
2016-06-13 16:50:02 +1000

    refactored getElementById

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 function main() {
-
-  var stickyColour = document.getElementById("colourchooser").value
-  var stickyMessage = document.getElementById("stickyMessage").value
+  var idGetter = function(id){
+    return document.getElementById(id);
+  }
+  var stickyColour = idGetter("colourchooser").value
+  var stickyMessage = idGetter("stickyMessage").value
   var newElement = document.createElement('p');
   newElement.className += "box";
   newElement.style.backgroundColor = stickyColour
   var textnode = document.createTextNode(stickyMessage);
   newElement.appendChild(textnode);
   document.querySelector(".container-homework").appendChild(newElement);
-  document.getElementById('stickyMessage').value = "";
+  idGetter('stickyMessage').value = "";
 
 }
